19d55194e4fb37b1aa1f6c0aa97e4124812d07b9
Sander Petersson <sanderp@fr24.com>
2023-03-15 09:22:58 +0100

    First commit

---
+++ custom.el
@@ -0,0 +1,48 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#191919" "#FF5E5E" "#468800" "#E9FDAC" "#8CDAFF" "#C586C0" "#85DDFF" "#d4d4d4"])
+ '(custom-safe-themes
+   '("6c531d6c3dbc344045af7829a3a20a09929e6c41d7a7278963f7d3215139f6a7" "846b3dc12d774794861d81d7d2dcdb9645f82423565bfb4dad01204fa322dbd5" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" default))
+ '(exwm-floating-border-color "#121212")
+ '(fci-rule-color "#515151")
+ '(highlight-tail-colors ((("#1d2416" "#1d2416") . 0) (("#232c30" "#202c30") . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#171F24" "#FFFFFF"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#171F24" "#468800"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#171F24" "#777778"))
+ '(objed-cursor-color "#FF5E5E")
+ '(package-selected-packages '(org-journal))
+ '(pdf-view-midnight-colors (cons "#d4d4d4" "#191919"))
+ '(rustic-ansi-faces
+   ["#191919" "#FF5E5E" "#468800" "#E9FDAC" "#8CDAFF" "#C586C0" "#85DDFF" "#d4d4d4"])
+ '(vc-annotate-background "#191919")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#468800")
+    (cons 40 "#7caf39")
+    (cons 60 "#b2d672")
+    (cons 80 "#E9FDAC")
+    (cons 100 "#efd98e")
+    (cons 120 "#f5b671")
+    (cons 140 "#FC9354")
+    (cons 160 "#e98e78")
+    (cons 180 "#d78a9c")
+    (cons 200 "#C586C0")
+    (cons 220 "#d8789f")
+    (cons 240 "#eb6b7e")
+    (cons 260 "#FF5E5E")
+    (cons 280 "#dd6464")
+    (cons 300 "#bb6a6b")
+    (cons 320 "#997071")
+    (cons 340 "#515151")
+    (cons 360 "#515151")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
